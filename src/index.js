7af661577faa57d028489618413acacdc6279b62
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2023-12-12 21:00:13 -0800

    end of weekend reading materials

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import Triangle from './triangle.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/styles.css';
+
 function handleTriangleForm(e) {
     e.preventDefault();
+    debugger;
     document.querySelector('#response').innerText = null;
     const length2 = parseInt(document.querySelector('#length2').value);
     const length1 = parseInt(document.querySelector('#length1').value);
